c10e24948f3a7aa1f523ce59114d1643cafc2e1c
Gandom <sudmandbita68@gmail.com>
2022-10-20 10:38:24 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+//feature 1
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='grid-container'>
+        <header>
+          <a href="http://www.google.com">React Redux Shopping Card</a>
+        </header>
+        <main>
+          All Products are here
+
+        </main>
+        <footer>
+  
+          <p> All Things Are Reserverd</p>
+          <p> All Things Are Reserverd</p>
+          <p> All Things Are Reserverd</p>
+
+
+        </footer>
+
+      </div>
+    )
+  }
+}
